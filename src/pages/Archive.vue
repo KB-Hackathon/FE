7a395c0cda07d1bcee1ff0c9d54538ed921332f6
Ian <105128049+minijae011030@users.noreply.github.com>
2025-08-27 02:54:03 +0900

    Design: 보관 페이지 퍼블리싱

--- src/pages/Archive.vue
+++ src/pages/Archive.vue
@@ -1,4 +1,160 @@
 <template>
-  <div>archive</div>
+  <div class="flex w-full flex-col">
+    <div class="w-full flex justify-between items-center mb-3">
+      <TypographyHead3>나의 위시리스트</TypographyHead3>
+      <TypographyP2 class="text-gray-500">
+        더보기
+      </TypographyP2>
+    </div>
+    <div class="w-full grid grid-cols-2 gap-2">
+      <div class="mb-1">
+        <div class="bg-slate-300 w-full h-[90px] rounded-t-md" />
+
+        <div class="pt-3 px-1">
+          <TypographyHead3>35% 달성</TypographyHead3>
+
+          <TypographyP1>맛있는 찰옥수수 냉동 ...</TypographyP1>
+        </div>
+        <div class="flex gap-2 mt-2 px-1">
+          <Badge class="bg-red-100 text-red-600">
+            마감임박
+          </Badge>
+          <Badge variant="secondary">
+            배송
+          </Badge>
+        </div>
+      </div>
+      <div class="mb-1">
+        <div class="bg-slate-300 w-full h-[90px] rounded-t-md" />
+
+        <div class="pt-3 px-1">
+          <TypographyHead3>35% 달성</TypographyHead3>
+
+          <TypographyP1>맛있는 찰옥수수 냉동 ...</TypographyP1>
+        </div>
+        <div class="flex gap-2 mt-2 px-1">
+          <Badge class="bg-red-100 text-red-600">
+            마감임박
+          </Badge>
+          <Badge variant="secondary">
+            배송
+          </Badge>
+        </div>
+      </div>
+      <div class="mb-1">
+        <div class="bg-slate-300 w-full h-[90px] rounded-t-md" />
+
+        <div class="pt-3 px-1">
+          <TypographyHead3>35% 달성</TypographyHead3>
+
+          <TypographyP1>맛있는 찰옥수수 냉동 ...</TypographyP1>
+        </div>
+        <div class="flex gap-2 mt-2 px-1">
+          <Badge class="bg-red-100 text-red-600">
+            마감임박
+          </Badge>
+          <Badge variant="secondary">
+            배송
+          </Badge>
+        </div>
+      </div>
+      <div class="mb-1">
+        <div class="bg-slate-300 w-full h-[90px] rounded-t-md" />
+
+        <div class="pt-3 px-1">
+          <TypographyHead3>35% 달성</TypographyHead3>
+
+          <TypographyP1>맛있는 찰옥수수 냉동 ...</TypographyP1>
+        </div>
+        <div class="flex gap-2 mt-2 px-1">
+          <Badge class="bg-red-100 text-red-600">
+            마감임박
+          </Badge>
+          <Badge variant="secondary">
+            배송
+          </Badge>
+        </div>
+      </div>
+    </div>
+    <div class="w-full flex justify-between items-center mb-3 mt-6">
+      <TypographyHead3>최근 본 상품 리스트</TypographyHead3>
+      <TypographyP2 class="text-gray-500">
+        더보기
+      </TypographyP2>
+    </div>
+    <div class="w-full grid grid-cols-2 gap-2">
+      <div class="mb-1">
+        <div class="bg-slate-300 w-full h-[90px] rounded-t-md" />
+
+        <div class="pt-3 px-1">
+          <TypographyHead3>35% 달성</TypographyHead3>
+
+          <TypographyP1>맛있는 찰옥수수 냉동 ...</TypographyP1>
+        </div>
+        <div class="flex gap-2 mt-2 px-1">
+          <Badge class="bg-red-100 text-red-600">
+            마감임박
+          </Badge>
+          <Badge variant="secondary">
+            배송
+          </Badge>
+        </div>
+      </div>
+      <div class="mb-1">
+        <div class="bg-slate-300 w-full h-[90px] rounded-t-md" />
+
+        <div class="pt-3 px-1">
+          <TypographyHead3>35% 달성</TypographyHead3>
+
+          <TypographyP1>맛있는 찰옥수수 냉동 ...</TypographyP1>
+        </div>
+        <div class="flex gap-2 mt-2 px-1">
+          <Badge class="bg-red-100 text-red-600">
+            마감임박
+          </Badge>
+          <Badge variant="secondary">
+            배송
+          </Badge>
+        </div>
+      </div>
+      <div class="mb-1">
+        <div class="bg-slate-300 w-full h-[90px] rounded-t-md" />
+
+        <div class="pt-3 px-1">
+          <TypographyHead3>35% 달성</TypographyHead3>
+
+          <TypographyP1>맛있는 찰옥수수 냉동 ...</TypographyP1>
+        </div>
+        <div class="flex gap-2 mt-2 px-1">
+          <Badge class="bg-red-100 text-red-600">
+            마감임박
+          </Badge>
+          <Badge variant="secondary">
+            배송
+          </Badge>
+        </div>
+      </div>
+      <div class="mb-1">
+        <div class="bg-slate-300 w-full h-[90px] rounded-t-md" />
+
+        <div class="pt-3 px-1">
+          <TypographyHead3>35% 달성</TypographyHead3>
+
+          <TypographyP1>맛있는 찰옥수수 냉동 ...</TypographyP1>
+        </div>
+        <div class="flex gap-2 mt-2 px-1">
+          <Badge class="bg-red-100 text-red-600">
+            마감임박
+          </Badge>
+          <Badge variant="secondary">
+            배송
+          </Badge>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { Badge } from '@/shared/components/ui/badge'
+import { TypographyHead3, TypographyP1, TypographyP2 } from '@/shared/components/ui/typography'
+</script>
