2ca1625c70795bf037e4b509f4ff14b56e90e121
Ian <105128049+minijae011030@users.noreply.github.com>
2025-08-29 04:12:18 +0900

    Feat: 완료된 공동구매 추가

--- src/features/user/ui/Join.vue
+++ src/features/user/ui/Join.vue
@@ -1,11 +1,22 @@
 <template>
-  <TypographyHead3>참여중인 공동구매 현황</TypographyHead3>
+  <TypographyHead3 class="mt-4">
+    참여중인 공동구매 현황
+  </TypographyHead3>
   <div class="flex flex-col mt-3 gap-2">
     <MediumProductCard />
     <MediumProductCard />
   </div>
+
+  <TypographyHead3 class="mt-4">
+    완료된 공동구매
+  </TypographyHead3>
+  <div class="flex flex-col mt-3 gap-2">
+    <MediumProductCard3 />
+    <MediumProductCard3 />
+  </div>
 </template>
 <script setup lang="ts">
 import MediumProductCard from '@/shared/components/molecules/MediumProductCard.vue'
+import MediumProductCard3 from '@/shared/components/molecules/MediumProductCard3.vue'
 import { TypographyHead3 } from '@/shared/components/ui/typography'
 </script>
