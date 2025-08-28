2389e22bf9950900357693eb970a6cbf86895014
Ian <105128049+minijae011030@users.noreply.github.com>
2025-08-29 04:14:53 +0900

    Design: 공통 컴포넌트 디자인 수정
    그림자 제거, bg-white 삽입

--- src/shared/components/ui/input/Input.vue
+++ src/shared/components/ui/input/Input.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { cn } from '@/lib/utils'
 import { useVModel } from '@vueuse/core'
-import type { HTMLAttributes } from 'vue'
+import { ref, type HTMLAttributes } from 'vue'
 
 const props = defineProps<{
   defaultValue?: string | number
@@ -12,7 +12,8 @@ const props = defineProps<{
 const emits = defineEmits<{
   (e: 'update:modelValue', payload: string | number): void
 }>()
-
+const el = ref<HTMLInputElement | null>(null)
+defineExpose({ focus: () => el.value?.focus(), el })
 const modelValue = useVModel(props, 'modelValue', emits, {
   passive: true,
   defaultValue: props.defaultValue,
@@ -21,7 +22,9 @@ const modelValue = useVModel(props, 'modelValue', emits, {
 
 <template>
   <input
+    ref="el"
     v-model="modelValue"
+    v-bind="$attrs"
     :class="
       cn(
         'flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ccmkt-main disabled:cursor-not-allowed disabled:opacity-50',
